d79b8667fdd58fcb414b7b3c46d152a536a0e1bd
Julia <107463763+juliavanvuren@users.noreply.github.com>
2022-10-04 11:44:57 +0200

    Deleted unnecessary files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Dictionary App</h1>
+      <input className="btn btn-primary" type="submit"></input>
+    </div>
+  );
+}
+
+
