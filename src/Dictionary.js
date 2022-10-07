ad85d576292a5a2918bb245439b30c27d479d9cb
Julia <107463763+juliavanvuren@users.noreply.github.com>
2022-10-07 12:55:39 +0200

    Added Photo component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,22 +2,32 @@ import React, { useState } from "react";
 import axios from "axios";
 import './Dictionary.css';
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
 
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
-    setResults(response.data[0]);
-    
+    setResults(response.data[0]); 
   }
   
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
-  }
+  
+
+  let pexelsApiKey = "563492ad6f91700001000001e964b71e6a904943969a4e7c104e717b";
+  let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+  let headers = {"Authorization" : `Bearer ${pexelsApiKey}`};
+  axios.get(pexelsApiUrl, { headers : headers } ).then(handlePexelsResponse);
+}
 
   function handleSearch(event){
     event.preventDefault();
@@ -45,6 +55,7 @@ export default function Dictionary(props) {
       </form>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
   
---
+++ src/Photos.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos(props) {
+  console.log(props);
+  if (props.photos) {
+  return (
+    <section className="Photos">
+      <div className="row">
+    {props.photos.map(function(photo,index) {
+      return (
+        <div className="col-4" key={index}><a href={photo.src.landscape} target="_blank" rel="noreferrer">
+      <img src={photo.src.landscape} alt={photo.alt} className="img-fluid img" /></a>
+      </div>
+      );
+    })}  
+    </div>
+    </section>
+  );
+} else {
+  return null;
+}
+} 
