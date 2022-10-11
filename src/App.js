418f055a3c0ba270017f7d2de7a8901172088b1c
Julia <107463763+juliavanvuren@users.noreply.github.com>
2022-10-11 15:21:02 +0200

    Changed colors, added links in footer

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
         <h1>Dictionary</h1>
           <Dictionary defaultKeyword="sunrise" />
 
-      <div className="app-footer">This project is coded by <a href="https://www.linkedin.com/in/juliavanvuren/" target="_blank" rel="noreferrer">Julia van Vuren</a>, open-sourced on GitHub &amp; hosted on Netlify</div>
+      <div className="app-footer">This project is coded by <a href="https://www.linkedin.com/in/juliavanvuren/" target="_blank" rel="noreferrer">Julia van Vuren</a>, open-sourced on <a href="https://github.com/juliavanvuren" target="_blank" rel="noreferrer">GitHub</a> &amp; hosted on <a href="https://jazzy-lolly-53da69.netlify.app/" target="_blank" rel="noreferrer">Netlify</a></div>
       </div>
     </div>
     </div>
